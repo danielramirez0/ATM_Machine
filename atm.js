0b84d79232082759478a48b23cdf94d1ced346b9
Daniel Ramirez <notused@notused.net>
2021-03-25 17:07:32 -0700

    Fixed atm functions. finished app flow on index

--- atm.js
+++ atm.js
@@ -1,19 +1,40 @@
 // const account = require("./account.js");
-
+const prompt = require("prompt-sync")();
 const getBalance = function (source) {
-  return source.acctBal;
+  console.log("\n\nHere is your available account balance\n\n" + source);
 };
 
-const withdraw = function (source, amt) {
-  return source.acctBal - amt;
+const withdraw = function (source) {
+  let withdrawal = parseFloat(prompt("Enter the amount you would like to take out:"));
+  if (isNaN(withdrawal) || withdrawal === "") {
+    console.log(":::::ALERT:::::\nINVALID ENTRY\nPLEASE, RETRY USING NUMBERS ONLY");
+    withdraw();
+  } else {
+    return source - withdrawal;
+  }
 };
 
-const deposit = function (source, amt) {
-  return source.acctBal + amt;
+const deposit = function (source) {
+  let deposit = parseFloat(prompt("Enter the amount you would like to deposit:"));
+  if (isNaN(deposit) || deposit === "") {
+    console.log(":::::ALERT:::::\nINVALID ENTRY\nPLEASE, RETRY USING NUMBERS ONLY");
+    deposit();
+  } else {
+    return source + deposit;
+  }
+};
+
+const validatePin = function (input, validNumber) {
+  if (parseInt(input) === validNumber) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 module.exports = {
   getBalance: getBalance,
   withdraw: withdraw,
   deposit: deposit,
+  validatePin: validatePin,
 };
--- index.js
+++ index.js
@@ -1,57 +1,52 @@
 "use strict";
-let accountInfo = require("./account.js");
 const prompt = require("prompt-sync")();
-
-const validatePin = function (input, valid) {
-  if (parseInt(input) === valid) {
-    return true;
-  } else {
-    return false;
-  }
-};
+const { getBalance: checkMoney, deposit } = require("./atm");
+const { withdraw: getMoney } = require("./atm");
+const { deposit: putMoney } = require("./atm");
+const { validatePin: checkPin } = require("./atm");
+const accountInfo = require("./account.js");
 
 function app(data) {
   console.log("___/<^>\\___ SUPER RAD BANK ___/<^>\\___");
   console.log(`Attemping to access account# ${data.acctNum}`);
   let response = prompt("Please enter your pin: ").toLowerCase();
-  if (validatePin(response, data.acctPin) === true) {
+  if (checkPin(response, data.acctPin) === true) {
     ATM(data);
   } else if (response === "quit") {
     console.log("Thank you for using our services!");
     console.log("___/<^>\\___ SUPER RAD BANK ___/<^>\\___");
     return;
   } else {
-    console.log(":::::ALERT:::::\nTHAT PIN IS NOT VALID\nPLEASE, RETRY OR TYPE QUIT TO EXIT");
+    console.log(":::::ALERT:::::\nYOUR ENTRY IS NOT VALID\nPLEASE, RETRY OR TYPE QUIT TO EXIT");
     return app(data);
   }
 }
 
 function ATM(data) {
-  const { getBalance: checkMoney } = require("./atm.js");
-  const { withdraw: getMoney } = require("./atm.js");
-  const { deposit: saveMoney } = require("./atm.js");
-  console.log("========= ATM MAIN MENU ==========\n---Choose your desired action---\n");
-  const mainMenu = "1: Check available balance.\n2: Make a deposit.\n3: Withdraw funds.\n4: Change account pin.\n5: Exit ATM Session";
-  console.log(mainMenu);
-  let action = parseInt(prompt("Selection: "));
+  let action;
+  console.log(
+    "\n\n========= ATM MAIN MENU ==========\n---Choose your desired action---\n1: Check available balance.\n2: Make a deposit.\n3: Withdraw funds.\n4: Change account pin.\n5: Exit ATM Session\n\n"
+  );
+  action = parseInt(prompt("Selection: "));
   switch (action) {
     case 1:
-      console.log("\n\nHere is your available account balance\n\n");
-      console.log(`$${checkMoney(accountInfo)}\n\n`);
+      checkMoney(data.acctBal);
       break;
     case 2:
-      console.log("mk dep");
+      data.acctBal = putMoney(data.acctBal);
+      checkMoney(data.acctBal);
       break;
     case 3:
-      console.log("mk with");
+      data.acctBal = getMoney(data.acctBal);
+      checkMoney(data.acctBal);
       break;
     case 4:
-      console.log("chng pin");
+      changePin();
       break;
     case 5:
-      console.log("exit");
+      console.log("Thank you for using our services!");
+      console.log("___/<^>\\___ SUPER RAD BANK ___/<^>\\___");
       return;
-      break;
     default:
       console.log("That choice is not one of the available options, please try again.");
       ATM(data);
@@ -59,7 +54,5 @@ function ATM(data) {
   }
   ATM(data);
 }
-
-function mainMenu(action) {}
 
 app(accountInfo);
